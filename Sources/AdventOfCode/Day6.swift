c0a863d457cbe214bde46e8cbcdda238e49e54cc
Peter Kovacs <peter@sanebox.com>
2020-12-06 08:40:14 -0500

    Day 6, but less stupid this time.

--- Sources/AdventOfCode/Day6.swift
+++ Sources/AdventOfCode/Day6.swift
@@ -5,16 +5,8 @@ import Algorithms
 import FootlessParser
 
 struct Day6: ParsableCommand {
-    static let input = Array(stdin).joined(separator: "\n")
-    static let parser =
-        oneOrMore(
-                oneOrMore(
-                    ( oneOrMore(oneOf("abcdefghijklmnopqrstuvwxyz")) <* char("\n") )
-                )
-                <* optional(char("\n"))
-        )
     func run() {
-        let groups = try! FootlessParser.parse(Self.parser, Self.input)
+        let groups = Array(stdin).joined(separator: "\n").components(separatedBy: "\n\n").map { $0.split(separator: "\n") }
         print("Part 1", groups.map { Set($0.joined()) }.map(\.count).sum)
         print("Part 2", groups.map { $0.reduce(Set("abcdefghijklmnopqrstuvwxyz")) { $0.intersection($1) }}.map(\.count).sum )
     }
